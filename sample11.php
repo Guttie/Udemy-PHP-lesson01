574e47a804d68a08b393932f1cd9a55b8496b65d
YutaroSakaguchi <90736959+Guttie@users.noreply.github.com>
2022-03-16 22:55:24 +0900

    [FINISH]連想配列

--- sample11.php
+++ sample11.php
@@ -1,4 +1,16 @@
 <?php
-$fruits = ['りんご', 'ぶどう'];
-echo $fruits[0];
+$fruits = [
+  'apple' => 'りんご',
+  'grape' => 'ぶどう',
+  'lemon' => 'レモン',
+  'tomato' => 'トマト',
+  'peach' => 'もも',
+];
 ?>
+<!-- 連想配列の内容を一覧表示させたい時 -->
+<dl>
+  <?php foreach ($fruits as $english => $japanese): ?>
+    <dt><?php echo $english; ?></dt>
+    <dd><?php echo $japanese; ?></dd>
+  <?php endforeach; ?>
+</dl>
